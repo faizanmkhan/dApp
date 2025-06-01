a8804f27b05d1ec6d0c42948c654a96defcbaf71
Lenovo WSLubuntu <systemd@wslubuntu.fr>
2025-06-01 06:26:35 +0200

    Human verification addded

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,19 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import { MiniKitProvider } from '@worldcoin/minikit-js/minikit-provider';
+export default async function Root({
+	children,
+}: Readonly<{
+	children: React.ReactNode
+}>) {
+	return (
+		<html lang="en">
+			<MiniKitProvider>
+				<body className={inter.className}>{children}</body>
+			</MiniKitProvider>
+		</html>
+	)
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,6 +4,42 @@ import Timer from '../components/Timer';
 import SessionTracker from '../components/SessionTracker';
 import RewardDisplay from '../components/RewardDisplay';
 import { useToast } from '@/hooks/use-toast';
+import { MiniKit, VerifyCommandInput, VerificationLevel, ISuccessResult } from '@worldcoin/minikit-js'
+
+const verifyPayload: VerifyCommandInput = {
+	action: 'voting-action', // This is your action ID from the Developer Portal
+	verification_level: VerificationLevel.Orb, // Orb | Device
+}
+
+const handleVerify = async () => {
+	if (!MiniKit.isInstalled()) {
+		return
+	}
+	// World App will open a drawer prompting the user to confirm the operation, promise is resolved once user confirms or cancels
+	const {finalPayload} = await MiniKit.commandsAsync.verify(verifyPayload)
+		if (finalPayload.status === 'error') {
+			return console.log('Error payload', finalPayload)
+		}
+
+		// Verify the proof in the backend
+		const verifyResponse = await fetch('/api/verify', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+			payload: finalPayload as ISuccessResult, // Parses only the fields we need to verify
+			action: 'start-timer',
+		}),
+	})
+
+	// TODO: Handle Success!
+	const verifyResponseJson = await verifyResponse.json()
+	if (verifyResponseJson.status === 200) {
+		console.log('Verification success!')
+	}  else {
+    console.error('Backend verification failed:', result)
+} }
 
 const Index = () => {
   const [isActive, setIsActive] = useState(false);
